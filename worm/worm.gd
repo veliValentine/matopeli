cf84cef7835de308a189f444d0c189985a548d07
unknown <nikkevanukas1995@gmail.com>
2024-06-17 13:59:11 +0300

    refactor: clean up movement

--- worm/worm.gd
+++ worm/worm.gd
@@ -1,17 +1,21 @@
 extends Area2D
 
-@export var speed = 200 # How fast the player will move (pixels/sec).
-@export var turn_speed = PI / 180 * 3 # How fast the worm will turn (degrees / sec)
+@export var velocity = Vector2(1,0)
+@export var speed = 200
+@export var turn_speed = PI / 180 * 3
+
 @export var left_turn_action = "p1_left"
 @export var right_turn_action = "p1_right"
 
-var screen_size # Size of the game window.
-var velocity = Vector2(1,0)
+var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
+	handle_turns(delta)
+
+func handle_turns(delta):
 	var turn_direction = 0
 	if Input.is_action_pressed(left_turn_action):
 		turn_direction -= turn_speed
@@ -23,6 +27,7 @@ func _process(delta):
 	position += velocity * delta
 	clamp_to_screen()
 	
+
 func clamp_to_screen():
 	if position.x > screen_size.x:
 		position.x = 0
